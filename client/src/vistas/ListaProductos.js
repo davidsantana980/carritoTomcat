8e2a07229a50ae71e33fd2cbc474be9cef603ef7
davidsantana980 <108551512+davidsantana980@users.noreply.github.com>
2023-09-29 17:16:18 -0400

    build del cliente se lanza desde el server

--- client/src/vistas/ListaProductos.js
+++ client/src/vistas/ListaProductos.js
@@ -1,108 +1,124 @@
-import { useState, useEffect, useCallback, useMemo } from "react"
+import {  Component } from "react"
 import { Button, Card, CardGroup, Col, Container, Row } from "react-bootstrap"
-import { useLocation, useNavigate } from "react-router-dom"
+import { Navigate, useLocation } from "react-router-dom"
 import Producto from "./modales/ProductoModal";
 
-export default function ListaProductos(){
-    //recibe los parametros
-    let nav = useNavigate();
-    let {state : params} = useLocation();
+class ClaseListaProductos extends Component{
     //copia los parametros a una variable local
-    let queryParams = useMemo(() => {
-        return {...params}
-    }, [params])
+    queryParams = {...this.props.params}
 
-    if(!!params){ 
-        //si hay parametros, borra los indefinidos
-        Object.keys(params).forEach(key => {
-            return !queryParams[key] && typeof(queryParams[key]) !== "boolean" ? delete queryParams[key] : {}
-        });
+    constructor(props) {
+        super(props);
+        this.state = {
+            listaDeProductos : [],
+            dataIsLoaded : false,
+            message : ""
+        };
+
+        if(!this.props.params) return <Navigate to={"/"}/>;
+
+        if(!!this.props.params){ 
+            //si hay parametros, borra los indefinidos
+            Object.keys(this.props.params).forEach(key => {
+                return !this.queryParams[key] && typeof(this.queryParams[key]) !== "boolean" ? delete this.queryParams[key] : {}
+            });
+        }
+    
     }
 
-    const [state, changeState] = useState({
-        listaDeProductos : [],
-        dataIsLoaded : false,
-        message : ""
-    })
+    async componentDidMount(){
+        try{
+            let url = `http://localhost:8080/api/productos?${new URLSearchParams(this.queryParams).toString()}` 
+
+            await fetch(url)
+            .then((res) => res.json()) //take the response string and turn it into a json array
+            .then((json) => { //take the json array from the previous step...
+                if(typeof json === "object" && json.length >= 1){
+                    return this.setState({
+                        listaDeProductos: json, //...and make our this.state.items<Array> == the JSON<Array> response
+                        dataIsLoaded:true //changed status
+                    })
+                }
     
-    let loadItems = useCallback(() => {
-        let url = `http://localhost:8080/api/productos?${new URLSearchParams(queryParams).toString()}` 
-        
-        fetch(url)
-        .then((res) => res.json()) //take the response string and turn it into a json array
-        .then((json) => { //take the json array from the previous step...
-            if(typeof json === "object" && json.length >= 1){
-                return changeState({
-                    listaDeProductos: json, //...and make our this.state.items<Array> == the JSON<Array> response
-                    dataIsLoaded:true //changed status
+                return this.setState({
+                    dataIsLoaded : false,
+                    message : "Ups! No hay resultados"
                 })
-            }
-
-            return changeState({
-                dataIsLoaded : false,
-                message : "Ups! No hay resultados"
+            })  
+            .catch((error) => {
+                throw new Error("Network response was not OK");
             })
-        })  
-    }, [queryParams])
+        }catch(error){
+            console.log(error)
+            this.setState({
+                dataIsLoaded:true //changed status
+            })
+        }
+    }
 
-    useEffect(() => {
-        if(!params) return nav("/", {replace : true});
-        return loadItems
-    }, [state.dataIsLoaded, loadItems, nav, params])
+    render(){
+        // if(!this.props.params) return nav("/", {replace : true});
+        
+        if(this.state.dataIsLoaded){
+            let productoCards = this.state.listaDeProductos.map((producto, index) => {
+                return (
+                        <Container fluid key={index} className="mb-2">
+                            <Card key={index} className="text-center">
+                                <Card.Body>
+                                    <Row className="mb-3">
+                                        <Col>
+                                            <Container fluid>
+                                                <Card.Title as={Button} className="btn-light">
+                                                    <Producto props={producto}/>
+                                                </Card.Title>
+                                                <Card.Text>
+                                                    {producto.descripcion ? (
+                                                        <span className="text-muted" >${producto.precio}</span>
+                                                    ) : (
+                                                        "Mira mas detalles"
+                                                    )}
+                                                </Card.Text>
+                                            </Container>    
+                                        </Col>
+                                    </Row>
+                                </Card.Body>
+                            </Card>
+                        </Container>
+                )
+            })
 
-    if(state.dataIsLoaded){
-        let productoCards = state.listaDeProductos.map((producto, index) => {
             return (
-                    <Container fluid key={index} className="mb-2">
-                        <Card key={index} className="text-center">
-                            <Card.Body>
-                                <Row className="mb-3">
-                                    <Col>
-                                        <Container fluid>
-                                            <Card.Title as={Button} className="btn-light">
-                                                <Producto props={producto}/>
-                                            </Card.Title>
-                                            <Card.Text>
-                                                {producto.descripcion ? (
-                                                    <span className="text-muted" >${producto.precio}</span>
-                                                ) : (
-                                                    "Mira mas detalles"
-                                                )}
-                                            </Card.Text>
-                                        </Container>    
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                        </Card>
+                <>
+                    <Container fluid className="mt-1 mb-2 p-3  col-lg-8 border bg-light">
+                        <span className="display-6">Resultados:</span>
                     </Container>
-            )
-        })
+                    <Container className="border col-md-12 col-lg-8 mb-5" >
+                        <CardGroup className="mt-3">
+                            {productoCards}
+                        </CardGroup>
+                    </Container>
+                </>
+            ) 
+
+
+        }
+
+        let message = "Espere..."
+
+        if(this.state.message){
+            message = this.state.message
+        }
 
         return (
-            <>
-                <Container fluid className="mt-1 mb-2 p-3  col-lg-8 border bg-light">
-                    <span className="display-6">Resultados:</span>
-                </Container>
-                <Container className="border col-md-12 col-lg-8 mb-5" >
-                    <CardGroup className="mt-3">
-                        {productoCards}
-                    </CardGroup>
-                </Container>
-            </>
-        ) 
-
-
+            <Container fluid className="mt-2 p-4  col-lg-8 border bg-light">
+                <h1>{message}</h1>
+            </Container>
+        )
     }
+}
 
-    let message = "Espere..."
+export default function ListaProductos(props){
+    const {state} = useLocation();
 
-    if(state.message){
-        message = state.message
-    }
-
-    return (
-        <Container fluid className="mt-2 p-4  col-lg-8 border bg-light">
-            <h1>{message}</h1>
-        </Container>
-    )
+    return <ClaseListaProductos {...props} params={state} />
 }
